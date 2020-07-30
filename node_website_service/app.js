f71f284d918b6d5acaff45c515cdf32435e4c8be
Steven Tian <yuant614@gmail.com>
2020-07-30 14:35:32 +0800

    implemented websocket for heartbeat message

--- node_website_service/app.js
+++ node_website_service/app.js
@@ -3,7 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const io = require('socket.io');
 
+let clients = [];
+
+//routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -37,5 +41,46 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//initialize socket.io
+// Create a Socket.IO instance, passing it our server
+  let socket = io(server);
+  socket.on('connection', (client) =>{
+        console.log('new client connected');
+        console.log('remote client\'s token is ' + client.handshake.query.token);
+        if(client.handshake.query.id != ''){
+                let id = client.handshake.query.token;
+                let c = clients[id];
+                if(typeof(c) != 'undefined' && c != null){
+                  if(c.handshake.query.token != client.handshake.query.token){
+                    console.log('token changed, client reconnected, update');
+                    //client reconnected, update
+                    clients[id] = client;
+                  }
+                }else{
+                  console.log('client not found, enqueue this one, id-->' + id);
+                  clients[id] = client;
+                }
+        }
+
+    client.on('message', (data)=>{
+      console.log('Received message from client!' , msg);
+      let msg = JSON.parse(data);
+      console.log('type -->' + msg.type);
+      switch(msg.type){
+        case 1000://heartbeat
+            let token =  client.handshake.query.token;
+            console.log('heartbeat message -->' +token);
+            let response = {};
+            response.type = 1000;
+            response.content = 'received hb msg from ' + token;
+            client.send(JSON.stringify(response));
+          break;
+      }
+    });
+
+  });
+
+
 
 module.exports = app;
